cdc33e8be8bd0c03bd7cf9908f98c7820ae1e130
aqionghhh <2667183507@qq.com>
2021-12-27 23:08:00 +0800

    2021/12/27

--- models/Article.js
+++ models/Article.js
@@ -2,22 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const db = require('../db/db');
 
-const CommentSchema = new Schema({
+const ArticleSchema = new Schema({
   userID: {//用户ID
     type: Schema.Types.ObjectId,
     ref: 'User'
   },
-  articleID: {//文章ID
-    type: Schema.Types.ObjectId,
-    ref: 'Acticle'
+  topic: {//标题
+    type: String,
   },
-  content: {//评论内容
+  content: {
+    type: String,
+  },
+  cover: {//图片
     type: String
   },
-  time: {//发表评论时间
+  date: {
     type: Date,
     default: Date.now
   }
 });
 
-module.exports = db.model('Comment', CommentSchema);
+module.exports = db.model('Article', ArticleSchema);
--- models/Follow.js
+++ models/Follow.js
@@ -2,22 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const db = require('../db/db');
 
-const CommentSchema = new Schema({
-  userID: {//用户ID
+const FollowSchema = new Schema({
+  firstID: {//被关注者id
     type: Schema.Types.ObjectId,
     ref: 'User'
   },
-  articleID: {//文章ID
+  secondID: {//关注者id
     type: Schema.Types.ObjectId,
-    ref: 'Acticle'
+    ref: 'User'
   },
-  content: {//评论内容
-    type: String
-  },
-  time: {//发表评论时间
+  time: {
     type: Date,
     default: Date.now
   }
 });
 
-module.exports = db.model('Comment', CommentSchema);
+module.exports = db.model('Follow', FollowSchema);
